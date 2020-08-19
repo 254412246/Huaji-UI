e25da87a22c5a60d789780aae8a178b511ee7aae
Administrator <254412246@qq.com>
2020-08-19 22:31:21 +0800

    添加菊花状态

--- src/button.vue
+++ src/button.vue
@@ -22,6 +22,11 @@ export default {
 }
 </script>
 <style lang="scss">
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
 .g-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
   border-radius: var(--border-radius); border: 1px solid var(--border-color);
   background: var(--button-bg);
@@ -36,5 +41,8 @@ export default {
     > .content { order: 1; }
     > .icon { order: 2; margin-right: 0; margin-left: .1em;}
   }
+}
+.loading{
+  animation: spin 2s infinite linear;
 }
 </style>
