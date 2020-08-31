e3f5457f915d72367229464237bfbbe8e52c84a7
Administrator <254412246@qq.com>
2020-08-31 15:37:24 +0800

    安装依赖和使用默认主题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,23 @@
 module.exports = {
     base: '/gulu-demo/',
-    theme: 'reco',
+    // theme: 'reco',
     title: '滑稽UI',
+
     description: '一个好用的UI框架',
     themeConfig: {
-        lastUpdated: 'Last Updated', // string | boolean
-        logo: '/public/images.png',
-        authorAvatar: '/public/huaji.svg',
+        logo: '/assets/huaji.svg',
+        noFoundPageByTencent: false,
         nav: [
-            {text: '主页', link: '/'},
-            {text: '文档', link: '/guide/'},
-            {text: '交流', link: 'https://google.com'},
+            {text: '主页', link: '/',icon: 'reco-home'},
+            {text: '文档', link: '/guide/',icon: 'reco-document'},
+            {text: '交流', link: 'https://github.com/254412246',icon: 'reco-github'},
         ],
         sidebar: [
             {
                 title: '入门',
                 children: [
                     '/install/',
-                    '/get-started/'
+                    '/startup/'
                 ]
             },
             {
