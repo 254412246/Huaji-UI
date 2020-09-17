1fea4c1b4c931a0e6e07c013816fd561bcfd1d83
Administrator <254412246@qq.com>
2020-09-17 20:30:03 +0800

    tabs 可以切换了

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -5,16 +5,48 @@
 </template>
 <script>
 export default {
-  name: 'GuluTabsItem',
-  props: {
-    disabled: {
-      type: Boolean,
-      default: false
+    name: 'GuluTabsItem',
+    inject: ['eventBus'],
+    data () {
+      return {
+        active: false
+      }
+    },
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      name: {
+        type: String | Number,
+        required: true
+      }
+    },
+    computed: {
+      classes () {
+        return {
+          active: this.active
+        }
+      }
+    },
+    created () {
+      this.eventBus.$on('update:selected', (name) => {
+        this.active = name === this.name;
+      })
+    },
+    methods: {
+      xxx () {
+        this.eventBus.$emit('update:selected', this.name)
+      }
     }
   }
-}
 </script>
-<style>
+<style scoped lang="scss">
 .tabs-item {
+  flex-shrink: 0;
+  padding: 0 1em;
+&.active {
+   background: red;
+ }
 }
 </style>
