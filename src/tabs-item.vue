fd5a2218603fc169dc83de44428961fffbfce54e
Administrator <254412246@qq.com>
2020-09-18 20:48:09 +0800

    触发 update:selected 事件的时候添加一个 item 数据

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -36,12 +36,13 @@ export default {
   },
   methods: {
     xxx () {
-      this.eventBus.$emit('update:selected', this.name)
+      this.eventBus.$emit('update:selected', this.name, this)
     }
   }
 }
 </script>
 <style lang="scss" scoped>
+$blue: blue;
 .tabs-item {
   flex-shrink: 0;
   padding: 0 1em;
@@ -50,7 +51,8 @@ export default {
   height: 100%;
   display: flex;
   &.active {
-    background: red;
+    color: $blue;
+    font-weight: bold;
   }
 }
 </style>
