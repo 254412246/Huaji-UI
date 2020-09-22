24622128075189a5a3be68b1f2cb790117630e33
Administrator <254412246@qq.com>
2020-09-22 22:48:00 +0800

    完成index.js 和 deploy.sh 尝试完成官网

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ import Icon from './src/icon'
 import Input from './src/input'
 import Row from './src/row'
 import Col from './src/col'
+import Tabs from './src/tabs/tabs'
+import TabsHead from './src/tabs/tabs-head'
+import TabsBody from './src/tabs/tabs-body'
+import TabsItem from './src/tabs/tabs-item'
+import TabsPane from './src/tabs/tabs-pane'
+import Layout from './src/layout'
+import Sider from './src/sider'
+import Content from './src/content'
+import Footer from './src/footer'
+import Header from './src/header'
+import Toast from './src/toastPlugin'
 
 export {
-    Button, ButtonGroup, Icon, Input, Row, Col
+    Button, ButtonGroup, Icon, Input, Row, Col, Layout, Header,
+    Content, Footer, Sider, Tabs, TabsHead, TabsBody, TabsItem, TabsPane, Toast
 }
